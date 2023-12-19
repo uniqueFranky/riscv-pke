b1f2d49a0d5bd58eac30464cefc4498ef594b92e
uniqueFranky <y_r_b@qq.com>
2023-12-19 20:20:53 +0800

    Update proc_file.c

--- kernel/proc_file.c
+++ kernel/proc_file.c
@@ -237,7 +237,7 @@ int do_rcwd(char *dst) {
     return 0;
   }
   int offset = 0;
-  for(int i = num - 1; i >= 0; i--) {
+  for(int i = num_name - 1; i >= 0; i--) {
     dst[offset++] = '/';
     strcpy(dst + offset, dir_names[i]);
     offset += strlen(dir_names[i]);
